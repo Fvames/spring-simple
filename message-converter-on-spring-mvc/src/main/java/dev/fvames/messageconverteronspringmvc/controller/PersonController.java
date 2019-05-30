11088f76c7813da24c7cba1f1254c42b22a10774
Fvames <warmhhands@gmail.com>
2019-05-30 09:37:11 +0800

    complete

--- message-converter-on-spring-mvc/src/main/java/dev/fvames/messageconverteronspringmvc/controller/PersonController.java
+++ message-converter-on-spring-mvc/src/main/java/dev/fvames/messageconverteronspringmvc/controller/PersonController.java
@@ -4,9 +4,11 @@ import dev.fvames.messageconverteronspringmvc.domain.Person;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 必须指明 @RequestBody
  * @version 2019/5/29 16:06
  */
 @RestController
@@ -19,15 +21,15 @@ public class PersonController {
     }
 
     @PostMapping(value = "/person/properties-to-json",
-            consumes = "application/properties+person",
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+            consumes = "application/properties+person",// 对应浏览器 Content-type
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE // 对应浏览器自描述信息 Accept
     )
-    public Person propertiesToJson(Person person) {
+    public Person propertiesToJson(@RequestBody Person person) {
         return person;
     }
 
     @PostMapping("/perosn/json-to-properties")
-    public Person jsonToProperties(Person person) {
+    public Person jsonToProperties(@RequestBody Person person) {
         return person;
     }
 
