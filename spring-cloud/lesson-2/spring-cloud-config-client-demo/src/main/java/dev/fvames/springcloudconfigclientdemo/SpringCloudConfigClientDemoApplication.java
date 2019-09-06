55a595788184b675dae5f9115b4ba47ad9d3ba3c
Fvames <warmhhands@gmail.com>
2019-09-06 14:00:51 +0800

    sc config client

---
+++ spring-cloud/lesson-2/spring-cloud-config-client-demo/src/main/java/dev/fvames/springcloudconfigclientdemo/SpringCloudConfigClientDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.fvames.springcloudconfigclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientDemoApplication.class, args);
+	}
+
+}
