93b32725ecd79e0fe7883565b91acde76fb426e5
Fvames <warmhhands@gmail.com>
2019-09-09 18:18:15 +0800

    init spring boot rabbitmq

---
+++ spring-boot-rabbitmq-demo/src/main/java/dev/fvames/springbootrabbitmqdemo/SpringBootRabbitmqDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.fvames.springbootrabbitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitmqDemoApplication.class, args);
+	}
+
+}
