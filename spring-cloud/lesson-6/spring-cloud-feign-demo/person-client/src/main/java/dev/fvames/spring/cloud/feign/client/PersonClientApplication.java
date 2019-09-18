98257d560b7fa1dd1185256e12faeb88dea2a72c
Fvames <warmhhands@gmail.com>
2019-09-18 14:36:15 +0800

    sc feign client init

---
+++ spring-cloud/lesson-6/spring-cloud-feign-demo/person-client/src/main/java/dev/fvames/spring/cloud/feign/client/PersonClientApplication.java
@@ -0,0 +1,22 @@
+package dev.fvames.spring.cloud.feign.client;
+
+import dev.fvames.spring.cloud.feign.api.service.PersonService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * 客户端启动类
+ *
+ * @version 2019/9/18 13:43
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(clients = PersonService.class)
+public class PersonClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonClientApplication.class, args);
+	}
+}
