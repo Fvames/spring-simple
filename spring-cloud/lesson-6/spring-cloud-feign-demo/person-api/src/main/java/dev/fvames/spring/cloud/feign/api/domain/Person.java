751fafb918d9b68c52dc5acad992e8d339374f59
Fvames <warmhhands@gmail.com>
2019-09-18 15:18:48 +0800

    sc feign server init

--- spring-cloud/lesson-6/spring-cloud-feign-demo/person-api/src/main/java/dev/fvames/spring/cloud/feign/api/domain/Person.java
+++ spring-cloud/lesson-6/spring-cloud-feign-demo/person-api/src/main/java/dev/fvames/spring/cloud/feign/api/domain/Person.java
@@ -1,16 +1,36 @@
 package dev.fvames.spring.cloud.feign.api.domain;
 
-import lombok.Data;
-
 /**
  * TODO 类描述
  *
  * @version 2019/9/18 13:32
  */
-@Data
 public class Person {
 
 	private Long id;
 	private String name;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
 }
--- spring-cloud/lesson-6/spring-cloud-feign-demo/person-server/src/main/java/dev/fvames/spring/cloud/feign/server/controller/PersonServerController.java
+++ spring-cloud/lesson-6/spring-cloud-feign-demo/person-server/src/main/java/dev/fvames/spring/cloud/feign/server/controller/PersonServerController.java
@@ -1,39 +1,33 @@
-package dev.fvames.spring.cloud.feign.client.controller;
+package dev.fvames.spring.cloud.feign.server.controller;
 
 import dev.fvames.spring.cloud.feign.api.domain.Person;
-import dev.fvames.spring.cloud.feign.api.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * TODO 类描述
  *
- *
- * @version 2019/9/18 13:43
+ * @version 2019/9/18 14:40
  */
 @RestController
-public class PersonController implements PersonService {
+public class PersonServerController {
 
-	private final PersonService personService;
-
-	@Autowired
-	public PersonController(PersonService personService) {
-		this.personService = personService;
-	}
+	private final Map<Long, Person> personMap = new ConcurrentHashMap<>();
 
 	@PostMapping("/person/save")
-	@Override
 	public boolean save(@RequestBody Person person) {
-		return personService.save(person);
+
+		return personMap.putIfAbsent(person.getId(), person) == null;
 	}
 
 	@GetMapping("/person/find/all")
-	@Override
 	public Collection<Person> findAll() {
-		return personService.findAll();
+		return personMap.values();
 	}
 }
