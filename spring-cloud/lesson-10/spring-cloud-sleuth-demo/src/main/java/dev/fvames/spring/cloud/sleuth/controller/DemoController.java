d7db0800bb2cdd2f68cc6ca9864be6a16d7c1a91
Fvames <warmhhands@gmail.com>
2019-09-27 13:27:00 +0800

    添加上报日志到 zipKin，从 eureka 发现服务并调用 zuul 接口方法

--- spring-cloud/lesson-10/spring-cloud-sleuth-demo/src/main/java/dev/fvames/spring/cloud/sleuth/controller/DemoController.java
+++ spring-cloud/lesson-10/spring-cloud-sleuth-demo/src/main/java/dev/fvames/spring/cloud/sleuth/controller/DemoController.java
@@ -2,8 +2,11 @@ package dev.fvames.spring.cloud.sleuth.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * TODO 类描述
@@ -14,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
 
+	@Value("${zuul-server-name}")
+	private String zuulServerName;
+	@Autowired
+	private RestTemplate restTemplate;
+
+
 	@GetMapping("")
 	public String index() {
 
@@ -22,5 +31,13 @@ private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.clas
 		LOGGER.info("{} index() : {}", getClass().getSimpleName(), returnValue);
 
 		return returnValue;
+	}
+
+	@GetMapping("/to/zuul/person-client/person/find/all")
+	public Object toZuul(String sourceName) {
+		LOGGER.info("spring-cloud-sleuth#toZuul()");
+
+		String url = "http://"+zuulServerName+"/person-client/person/find/all";
+		return restTemplate.getForObject(url, Object.class);
 	}
 }
