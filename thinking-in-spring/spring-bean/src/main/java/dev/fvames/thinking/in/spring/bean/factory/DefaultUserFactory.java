07670d9ccaed50ae9c4716624ea4cc1a56f868fa
Fvames <warmhhands@gmail.com>
2020-03-04 23:47:29 +0800

    initialization and destroy

--- thinking-in-spring/spring-bean/src/main/java/dev/fvames/thinking/in/spring/bean/factory/DefaultUserFactory.java
+++ thinking-in-spring/spring-bean/src/main/java/dev/fvames/thinking/in/spring/bean/factory/DefaultUserFactory.java
@@ -1,7 +1,62 @@
 package dev.fvames.thinking.in.spring.bean.factory;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * 默认 {@link UserFactory} 实现
+ * {@link InitializingBean#afterPropertiesSet()} 初始化 Bean
+ * <p>
+ *     初始化优先级顺序：
+ *     1. @PostConstruct
+ *     2. InitializingBean#afterPropertiesSet()
+ *     3. 自定义
+ * </p>
+ *
+ * {@link DisposableBean#destroy()} 销毁 Bean
+ * <p>
+ *     销毁优先级顺序
+ *     1.@PreDestroy
+ *     2.DisposableBean#destroy()
+ *     3.自定义销毁
+ * </p>
+ *
  */
-public class DefaultUserFactory implements UserFactory {
+public class DefaultUserFactory implements UserFactory, InitializingBean, DisposableBean {
+
+    // 初始化
+    @PostConstruct
+    public void doInit() {
+        System.out.println("@PostConstruct 初始化中 " );
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("InitializingBean#afterPropertiesSet() 初始化中");
+    }
+
+    // 自定义初始化
+    public void initUserFactory() {
+        System.out.println("自定义 initUserFactory() 初始化中");
+    }
+
+    // 销毁
+    @PreDestroy
+    public void doDestroy() {
+        System.out.println("@PreDestroy 销毁中");
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean#destroy() 销毁中");
+    }
+
+    // 自定义销毁
+    public void destroyUserFactory() {
+        System.out.println("自定义 destroyUserFactory() 销毁中");
+    }
 }
---
+++ thinking-in-spring/spring-bean/src/main/java/dev/fvames/thinking/in/spring/bean/initialization/BeanInitializationAndDestroyDemo.java
@@ -0,0 +1,39 @@
+package dev.fvames.thinking.in.spring.bean.initialization;
+
+import dev.fvames.thinking.in.spring.bean.factory.DefaultUserFactory;
+import dev.fvames.thinking.in.spring.bean.factory.UserFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+/**
+ * Bean 初始化
+ */
+@Configuration
+public class BeanInitializationAndDestroyDemo {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(BeanInitializationAndDestroyDemo.class);
+
+        // 刷新应用上下文
+        applicationContext.refresh();
+        // 延迟初始化时先打印，然后打印初始化
+        System.out.println("Spring 应用上下文已启动 \n");
+
+        // 依赖查找 UserFactory
+        UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+        System.out.println(userFactory + "\n");
+
+        System.out.println("Spring 应用上下文准备关闭 \n");
+        // 关闭应用上下文
+        applicationContext.close();
+        System.out.println("Spring 应用上下文已关闭");
+    }
+
+    @Lazy
+    @Bean(initMethod = "initUserFactory", destroyMethod = "destroyUserFactory")
+    public UserFactory userFactory(){
+        return new DefaultUserFactory();
+    }
+}
